1332a9a53a9ab475920e58e811953404b35a1c68
thu.pham <thu.pham@eastagile.com>
2018-07-04 12:23:22 +0700

    init project

---
+++ demoProjectObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demoProjectObjectiveC
+//
+//  Created by ea on 7/4/18.
+//  Copyright © 2018 ea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demoProjectObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demoProjectObjectiveC
+//
+//  Created by ea on 7/4/18.
+//  Copyright © 2018 ea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
